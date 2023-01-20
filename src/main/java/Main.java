2e80c241a65274116b3583861a244802140854ce
janithPriyankara <56631224+janithPriyankara@users.noreply.github.com>
2023-01-20 10:06:00 +0530

    Updated with variable

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,8 +14,12 @@ import io.nats.client.api.StreamInfo;
 import io.nats.client.impl.NatsMessage;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
@@ -25,15 +29,30 @@ public class Main {
   public static void main(String[] args)
       throws IOException, InterruptedException, JetStreamApiException, ExecutionException {
 
-    Connection nc = Nats.connect("nats://local:vvNGFu3HRk3PMjZHsztlsCZ9MoUwTGzR@0.0.0.0:37733");
+    String userName = "local";
+    String password = "vvNGFu3HRk3PMjZHsztlsCZ9MoUwTGzR";
+    String host = "0.0.0.0";
+    String port = "37733";
+    String streamName = "test_stream";
+    String subjectName = "test_subject_1";
 
+
+    Connection nc = Nats.connect("nats://"+userName+":"+password+ "@"+host+":"+port);
     JetStream js = nc.jetStream();
-    byte[] messsageBytes = "go fuck yourself".getBytes(StandardCharsets.UTF_8);
+
+    LocalTime currentTime = LocalTime.now();
+    String timeStamp = currentTime.toString();
+
+    byte[] messsageBytes = ("mock message from publisher "+ timeStamp).getBytes(StandardCharsets.UTF_8);
     PublishOptions po = PublishOptions.builder()
-        .stream("test_stream")
+        .stream(streamName)
         .build();
 
-    PublishAck pa = js.publish("test_subject_1", messsageBytes, po);
+    for (int i = 0; i <10;i++){
+      PublishAck pa = js.publish(subjectName, messsageBytes, po);
+    }
+
+
     nc.close();
   }
 }
